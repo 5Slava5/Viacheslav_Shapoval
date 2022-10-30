40df45671f58c6e9d15ce9d03cf7c52c708c4dc8
Viacheslav Shapoval <shapoval5viacheslav@gmail.com>
2022-10-30 16:03:58 +0200

    Commit 6. Add into the function "*" "/"

--- dz_8/py_charm.py
+++ dz_8/py_charm.py
@@ -3,6 +3,9 @@
 def mane_function(x, y):
     print(f'Результат x + y = {x + y} ')
     print(f'Результат x - y = {x - y} ')
+    print(f'Результат x * y = {x * y} ')
+    print(f'Результат x / y = {x / y} ')
+
 
 
 mane_function(int(input('Введіть число х: ')), int(input('Введіть число у: ')))
