a789b441966e7b7fd943c790d4f89e78fe719056
Viacheslav Shapoval <shapoval5viacheslav@gmail.com>
2022-10-30 16:02:06 +0200

    Commit 5. Add into the function "+" "-"

--- dz_8/py_charm.py
+++ dz_8/py_charm.py
@@ -1,8 +1,9 @@
 # Функція виконує арифметичні обчислювання
 
-def mane_function():
-    pass
+def mane_function(x, y):
+    print(f'Результат x + y = {x + y} ')
+    print(f'Результат x - y = {x - y} ')
 
 
-mane_function()
+mane_function(int(input('Введіть число х: ')), int(input('Введіть число у: ')))
 
