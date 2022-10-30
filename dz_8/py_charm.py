6c713be95f29d548f8a80e70cc83d1081242b72d
Viacheslav Shapoval <shapoval5viacheslav@gmail.com>
2022-10-30 15:32:45 +0200

    Commit 4. Remove blank li

---
+++ dz_8/py_charm.py
@@ -0,0 +1,7 @@
+# Функція з математичними операціями
+
+def mane_function(x, y):
+    print(f'Результат x * y = {x * y} ')
+
+
+mane_function(int(input('Введіть число х: ')), int(input('Введіть число у: ')))
