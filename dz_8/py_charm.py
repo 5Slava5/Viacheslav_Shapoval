ca99a5df6c0d7e1cb86c7c40a54811114a1a6a22
Viacheslav Shapoval <shapoval5viacheslav@gmail.com>
2022-10-30 16:17:32 +0200

    Commit 7. Create same file py_charm.py in test branch

---
+++ dz_8/py_charm.py
@@ -0,0 +1,11 @@
+# Функція виконує арифметичні обчислювання
+
+def mane_function(x, y):
+    print(f'Результат x + y = {x + y} ')
+    print(f'Результат x - y = {x - y} ')
+    print(f'Результат x * y = {x * y} ')
+    print(f'Результат x / y = {x / y} ')
+
+
+
+mane_function(int(input('Введіть число х: ')), int(input('Введіть число у: ')))
