d3a4c7cabb41f52138c228c8d3bb3aa1894e8dfe
Viacheslav Shapoval <shapoval5viacheslav@gmail.com>
2022-11-12 21:22:30 +0200

    Commit 2. Created a server dz_11_1_server.py

---
+++ dz_11/dz_11_1_server.py
@@ -0,0 +1,14 @@
+# Сервер для обміну повідомленнями
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(('localhost', 55000))
+sock.listen(10)
+print('Server is running, please, press ctrl+c to stop')
+while True:
+    conn, addr = sock.accept()
+    print('connected:', addr)
+    data = conn.recv(1024)
+    print(str(data))
+    conn.send(data.upper())
+conn.close()
